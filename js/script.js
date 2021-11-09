3f0de561285e16d481926f5d96fb89236743137d
Luca Sunseri <89150843+LucaSunseri@users.noreply.github.com>
2021-11-09 15:03:52 +0100

    Fase alpha del gioco

--- js/script.js
+++ js/script.js
@@ -8,8 +8,71 @@ Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei num
 
 
 
+const numeri = myRandomInts(5, 10, 15);
+
+const displayNumeri = document.querySelector('.numeri');
+
+const displayRisultato = document.querySelector('.risultato');
+
+
+displayNumeri.innerHTML = `
+  ${numeri.join(' - ')}
+`;
+
+const intervalloTempoGioco = setInterval(function() {
+
+  displayNumeri.classList.add('d-none');
+
+},5000);
+
+
+setTimeout(function() {
+
+  const numeriUtente = [];
+
+  for (let i = 0; i < 5; i++) {
+
+    const numeroUtente = parseInt(prompt('Inserisci i numeri'));
+    // console.log(numeroUtente);
+
+    numeriUtente.push(numeroUtente);
+  
+    // console.log(numeriUtente);
+  }
+
+  console.log(numeri);
+  console.log(numeriUtente);
+
+
+  let intersection = numeriUtente.filter(x => numeri.includes(x));
+
+  console.log(intersection);
+
+  displayRisultato.innerHTML = `
+  I numeri da te inseriti sono ${numeriUtente.join(' - ')}, <br>
+  ma quelli indovinati sono ${intersection.join(' - ')} <br>
+  in totale hai totalizzato ${intersection.length} punti!
+  `;
+
+},5200);
 
 
 
 
+// Funzione che genera un array di un tot. numeri(quantity) da x(min) a y(max) inclusi;
+function myRandomInts(quantity, min, max) {
 
+  const arrayNumeri = [];
+
+  while(arrayNumeri.length < quantity) {
+
+    let numero = Math.floor(Math.random() * (max - min + 1) + min);
+
+    if (!(arrayNumeri.includes(numero))) {
+      arrayNumeri.push(numero);
+    }
+
+  }
+
+  return arrayNumeri;
+}
